ed60156c7d1289260996a05f212b34d1247abe47
VileDeveloper <vladimirlitov131127@gmail.com>
2024-03-13 00:41:44 +0500

    feat: refactor by review

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,15 @@ module ApplicationHelper
       'alert-info'
     end
   end
+
+  def nav_menu_item(name, path = '#', options = {})
+    class_names = class_names('nav-link', 'link-dark', { active: current_page?(path) })
+    assembled_options = options.merge(class: class_names)
+
+    tag.li class: 'nav-item' do
+      link_to path, assembled_options do
+        name
+      end
+    end
+  end
 end
